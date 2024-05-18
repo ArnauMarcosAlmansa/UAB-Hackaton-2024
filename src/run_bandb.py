6c882c912f25e46d8a37b00fbd6ce79b0c3e3f3e
Arnau Marcos Almansa <arnaumarcosalmansa@gmail.com>
2024-05-19 00:48:40 +0200

    cambios al output

--- src/run_bandb.py
+++ src/run_bandb.py
@@ -4,10 +4,20 @@ import numpy as np
 
 from src.algorithms.bandb import salesman_track_branch_and_bound
 from src.algorithms.workday_splitting import workday_splitting
-from src.data.data import load_from_xlsx
-from src.utils import skip
+from src.data.data import load_from_xlsx, Town
+from src.utils import skip, print_path_splits
 
 ruta2, ruta4, ruta5 = load_from_xlsx("../data/Dades_Municipis.xlsx")
+
+girona, barcelona, tarragona = [Town(batch=0, block=0, region="", ine_code="", name="Carrer Indústria, 0, 17457 Riudellots de la Selva, Girona", population=0,
+                    min_stay_in_seconds=0),
+               Town(batch=0, block=0, region="", ine_code="", name="Av les Garrigues, 9, 08820, El Prat de Llobregat", population=0,
+                    min_stay_in_seconds=0),
+               Town(batch=0, block=0, region="", ine_code="", name="Pol. Industrial Riu Clar C/Sofre, parcel·la 131 – nau 30 43006 Tarragona", population=0,
+                    min_stay_in_seconds=0)]
+
+bases = {"tarragona": tarragona, "girona": girona, "barcelona": barcelona}
+
 
 for origin_name, batch, ruta in zip(["tarragona", "girona", "barcelona"], [2, 4, 5], [ruta2, ruta4, ruta5]):
     for block in [1, 2, 3, 4]:
@@ -19,11 +29,16 @@ for origin_name, batch, ruta in zip(["tarragona", "girona", "barcelona"], [2, 4,
         print(f"BLOQUE = {block}")
 
         for dist, path in skip(10, salesman_track_branch_and_bound(distance_matrix)):
+            towns = [t for t in ruta if t.block == block]
             splits = list(
-                workday_splitting(path, distance_matrix, origin_distances, [t for t in ruta if t.block == block]))
+                workday_splitting(path, distance_matrix, origin_distances, towns))
 
             for split in splits:
                 print(f"\tSPLIT = {split[1]}\tDURATION = {split[0]}")
+
+            print("\tPATH:")
+
+            print_path_splits(splits, towns, bases[origin_name], distance_matrix, origin_distances)
 
             if sum(map(lambda s: s[0], splits)) > 60 * 60 * 39 or len(splits) > 5:
                 print("MAL")
--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,8 @@
+import numpy as np
+
+from src.data.data import Town
+
+
 def skip(n: int, iter):
     for i in range(n):
         try:
@@ -7,3 +12,33 @@ def skip(n: int, iter):
 
     for elem in iter:
         yield elem
+
+
+def seconds_to_time_str(seconds: int) -> str:
+    secs = seconds % 60
+    minutes = seconds // 60
+    mins = minutes % 60
+    hours = minutes // 60
+
+    return f"{hours:02} h {mins:02} min {secs:02} s"
+
+
+def print_path_split(split, towns: list[Town], origin: Town, distance_matrix: np.ndarray, origin_distances: np.ndarray):
+    print(f"\t\t{origin.name}")
+    print(f"\t\t>>> {seconds_to_time_str(origin_distances[split[1][0]])}")
+    for part1, part2 in zip(split[1][0:-2], split[1][1:-1]):
+        print(f"\t\t{towns[part1].name}, {towns[part1].region} --- {seconds_to_time_str(towns[part1].min_stay_in_seconds)}")
+        print(f"\t\t>>> {seconds_to_time_str(distance_matrix[part1, part2])}")
+
+    print(f"\t\t{towns[split[1][-1]].name}, {towns[split[1][-1]].region} --- {seconds_to_time_str(towns[split[1][-1]].min_stay_in_seconds)}")
+    print(f"\t\t>>> {seconds_to_time_str(origin_distances[split[1][-1]])}")
+    print(f"\t\t{origin.name}")
+    print(f"\t\tTOTAL: {seconds_to_time_str(split[0])}")
+
+
+
+
+def print_path_splits(splits, towns: list[Town], origin: Town, distance_matrix: np.ndarray, origin_distances: np.ndarray):
+    for split in splits:
+        print_path_split(split, towns, origin, distance_matrix, origin_distances)
+        print()
